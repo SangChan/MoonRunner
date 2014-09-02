8e8109db2464177a8ee1b7f8d61e532399f53e30
SangChan Lee <sangchan82@gmail.com>
2014-09-02 11:59:50 +0900

    Initial Commit

---
+++ MoonRunner/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  MoonRunner
+//
+//  Created by SangChan on 2014. 9. 2..
+//  Copyright (c) 2014년 sangchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ MoonRunner/MasterViewController.h
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.h
+//  MoonRunner
+//
+//  Created by SangChan on 2014. 9. 2..
+//  Copyright (c) 2014년 sangchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
